b6fa8a065a5312e5f94d0f1057be78ac2992a842
srinjoy0212 <115336090+srinjoy0212@users.noreply.github.com>
2024-11-30 17:37:43 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
-// const BASE_URL = "https://trips-travel-server.vercel.app/api";
-const BASE_URL = "http://localhost:3050/api";
+const BASE_URL = "https://shivam-tour-travels.onrender.com/api";
+// const BASE_URL = "http://localhost:3050/api";
 //
 export default BASE_URL;
